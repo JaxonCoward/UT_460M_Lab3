5a1242ece05d33a7b90b3f34a443306893244f03
JaxonCoward <jaxon@DESKTOP-GSOR9ME.localdomain>
2021-03-10 21:18:32 -0600

    Initial fitbit module

---
+++ fitbit.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/10/2021 09:16:23 PM
+// Design Name: 
+// Module Name: fitbit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fitbit(
+    input CLK
+    );
+    
+
+    initial begin
+        
+    end
+
+    always @(posedge CLK) begin
+    
+    end
+
+    
+    
+endmodule
